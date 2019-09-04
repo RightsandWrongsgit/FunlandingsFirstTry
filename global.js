3b8135c5a6cfb8d644e3a156f71169110d71d83c
RightsandWrongsgit <48891049+RightsandWrongsgit@users.noreply.github.com>
2019-09-04 17:19:51 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.funlandings_subtheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
